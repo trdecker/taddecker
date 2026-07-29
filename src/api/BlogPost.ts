c290d50e5439aac5e216af4d774387d75ca4ee71
Tad Decker <tdecker234@gmail.com>
2026-07-29 11:59:29 -0600

    api/BlogPost.ts

---
+++ src/api/BlogPost.ts
@@ -0,0 +1,26 @@
+import type { BlogPost } from '../../shared/types';
+import { client } from './client';
+
+// ##### GET ##### //
+
+export async function getBlogPosts(): Promise<BlogPost[]> {
+  const { data, errors } = await client.models.BlogPost.list();
+
+  if (errors) {
+    throw new Error("Error loading blog posts");
+  }
+
+  return data;
+}
+
+export async function getBlogPostFromSlug(slug: string): Promise<BlogPost | undefined> {
+  const { data, errors } = await client.models.BlogPost.listBlogPostBySlug({
+    slug,
+  });
+
+  if (errors) {
+    throw new Error("Error loading blog post");
+  }
+
+  if (data?.length > 0) return data[0];
+}
