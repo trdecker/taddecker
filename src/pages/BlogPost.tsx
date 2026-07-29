d1ee4b635e9b73f294aaeb37f4b5744ec4e7f7b4
Tad Decker <tdecker234@gmail.com>
2026-07-29 12:00:07 -0600

    pages/BlogPost.tsx

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
+import { getBlogPostFromSlug } from "../api/BlogPost";
+import type { BlogPost } from "../../shared/types";
 
 
 export default function BlogPostPage() {
-  const { id } = useParams();
+  const { slug } = useParams();
+  const [blogPost, setBlogPost] = useState<BlogPost>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    async function loadBlogPost(slug: string) {
+      try {
+        const loadedBlogPost = await getBlogPostFromSlug(slug);
+        if (loadedBlogPost) setBlogPost(loadedBlogPost);
+        setLoading(false);
+      } catch (e) {
+        console.error(e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (slug) {
+      loadBlogPost(slug);
+    }
+  }, [slug]);
+
+  if (loading) return (<>Loading...</>);
+
+  if (!blogPost) return (<>Blog post not found</>);
 
   return(<>
-    {id}
-  
+    {blogPost.title ?? "NOT FOUND"}
+    <p>{blogPost.content}</p>
   </>)
 }
