8f9f4637d7d0793e5c42b4cf2074c4b6a7fc89f7
Tad Decker <tdecker234@gmail.com>
2026-07-31 14:56:57 -0600

    Missing Amplify.configure() bug fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,8 @@
 import { generateClient } from 'aws-amplify/data';
 import type { Schema } from '../../amplify/data/resource';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 export const client = generateClient<Schema>({ authMode: 'userPool' });
