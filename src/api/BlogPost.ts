f9820cbcfa9cbbc5262db5457c509086d8a18fa0
Tad Decker <tdecker234@gmail.com>
2026-07-31 14:56:47 -0600

    Blog post api permission fix

--- src/api/BlogPost.ts
+++ src/api/BlogPost.ts
@@ -4,7 +4,7 @@ import { client } from './client';
 // ##### GET ##### //
 
 export async function getBlogPosts(): Promise<BlogPost[]> {
-  const { data, errors } = await client.models.BlogPost.list();
+  const { data, errors } = await client.models.BlogPost.list({ authMode: 'apiKey'});
 
   if (errors) {
     throw new Error("Error loading blog posts");
@@ -16,6 +16,8 @@ export async function getBlogPosts(): Promise<BlogPost[]> {
 export async function getBlogPostFromSlug(slug: string): Promise<BlogPost | undefined> {
   const { data, errors } = await client.models.BlogPost.listBlogPostBySlug({
     slug,
+  }, {
+    authMode: 'apiKey',
   });
 
   if (errors) {
