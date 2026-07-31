212ac506ece90e9f63a253cb13d15a9808a91de8
Tad Decker <tdecker234@gmail.com>
2026-07-31 14:57:47 -0600

    Basic blog posts list in Blog.tsx

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import type { BlogPost } from "../../shared/types";
 import { getBlogPosts } from "../api/BlogPost";
 import { Link } from "react-router-dom";
-import { ROUTES } from "../../shared/routes";
 
 export default function BlogPage() {
   const [blogPosts, setBlogPosts] = useState<BlogPost[]>([]);
@@ -20,18 +19,31 @@ export default function BlogPage() {
     loadMostRecent();
   }, []);
 
+  function formatDate(dateString: string) {
+    return (new Date(dateString)).toLocaleDateString();
+  }
+
   return (<div className="flex flex-col p-4 md:p-8 lg:p-12">
     <h1 className="pt-16">Blog</h1>
 
       <div className="flex flex-col justify-center mt-8">
         {!blogPosts.length && 'No posts yet'}
-        {blogPosts.map(post =>
+        {blogPosts.map((post: BlogPost, index: number) => <div key={post.id}>
+          {index > 0 && (
+            <hr className="my-4 mx-16" />
+          )}
           <Link
             key={post.id}
-            to={`${ROUTES}/${post.slug}`}
+            to={`${post.slug}`}
+            className="flex flex-row justify-between"
           >
-            {post.title}
+            <span>
+              {post.title}
+            </span>
+            <span>{post.excerpt}</span>
+            <span>{post.publishedAt ? formatDate(post.publishedAt) : ''}</span>
           </Link>
+        </div>
         )}
       </div>
   </div>);
